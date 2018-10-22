850a298eb8917e2caa0f47afe57b23b0c864fc5c
Vlad Mihalcea <mihalcea.vlad@gmail.com>
2018-10-22 08:35:37 +0300

    Add types for java.time.Year and java.time.Month #51

---
+++ hibernate-types-52/src/main/java/com/vladmihalcea/hibernate/type/basic/YearType.java
@@ -0,0 +1,36 @@
+package com.vladmihalcea.hibernate.type.basic;
+
+import com.vladmihalcea.hibernate.type.basic.internal.YearTypeDescriptor;
+import org.hibernate.type.AbstractSingleColumnStandardBasicType;
+import org.hibernate.type.descriptor.sql.SmallIntTypeDescriptor;
+
+import java.time.Year;
+
+/**
+ * Maps a Java {@link Year} object to an {@code INT} column type.
+ * <p>
+ * For more details about how to use it, check out <a href="TODO">this article</a> on <a href="https://vladmihalcea.com/">vladmihalcea.com</a>.
+ *
+ * @author Vlad Mihalcea
+ */
+public class YearType
+        extends AbstractSingleColumnStandardBasicType<Year> {
+
+    public static final YearType INSTANCE = new YearType();
+
+    public YearType() {
+        super(
+                SmallIntTypeDescriptor.INSTANCE,
+                YearTypeDescriptor.INSTANCE
+        );
+    }
+
+    public String getName() {
+        return "year";
+    }
+
+    @Override
+    protected boolean registerUnderJavaType() {
+        return true;
+    }
+}
