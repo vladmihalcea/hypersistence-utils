f6ffd0c894ce2ab6b85611e7129f07b78910af0b
elkayssin <nazirlkc@gmail.com>
2018-11-29 16:06:46 -0500

    Add support for postgresql enum array types for Hibernate 4.3

--- hibernate-types-43/src/test/java/com/vladmihalcea/hibernate/type/model/BaseEntity.java
+++ hibernate-types-43/src/test/java/com/vladmihalcea/hibernate/type/model/BaseEntity.java
@@ -4,9 +4,11 @@ import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 import javax.persistence.Version;
 
+import org.hibernate.annotations.Parameter;
 import org.hibernate.annotations.TypeDef;
 import org.hibernate.annotations.TypeDefs;
 
+import com.vladmihalcea.hibernate.type.array.EnumArrayType;
 import com.vladmihalcea.hibernate.type.array.IntArrayType;
 import com.vladmihalcea.hibernate.type.array.StringArrayType;
 import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
@@ -20,6 +22,9 @@ import com.vladmihalcea.hibernate.type.json.JsonStringType;
 @TypeDefs({
         @TypeDef(name = "string-array", typeClass = StringArrayType.class),
         @TypeDef(name = "int-array", typeClass = IntArrayType.class),
+        @TypeDef(name = "sensor-state-array", typeClass = EnumArrayType.class, parameters = {
+            @Parameter(name = EnumArrayType.TYPE_DEF_NAME, value = "sensor-state-array"),
+            @Parameter(name = EnumArrayType.TYPE_DB_NAME, value = "sensor_state")} ),
         @TypeDef(name = "json", typeClass = JsonStringType.class),
         @TypeDef(name = "jsonb", typeClass = JsonBinaryType.class),
         @TypeDef(name = "jsonb-node", typeClass = JsonNodeBinaryType.class),
