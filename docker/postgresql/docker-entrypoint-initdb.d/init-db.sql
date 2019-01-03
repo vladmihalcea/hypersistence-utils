7ace28de5206afc8ed0db54347d1ebf16a16a5d3
Nikita Konev <nikit.cpp@yandex.ru>
2019-01-03 06:00:04 +0300

    add test for PostgreSQL enum array

--- docker/postgresql/docker-entrypoint-initdb.d/init-db.sql
+++ docker/postgresql/docker-entrypoint-initdb.d/init-db.sql
@@ -1,2 +1,16 @@
 alter role postgres password 'admin';
 create database high_performance_java_persistence with owner postgres;
+
+\c high_performance_java_persistence postgres;
+
+create schema if not exists auth;
+CREATE TYPE auth.user_role AS ENUM (
+    'ROLE_ADMIN',
+    'ROLE_USER'
+);
+
+CREATE TABLE auth.users (
+  id BIGSERIAL PRIMARY KEY,
+	username VARCHAR(50) NOT NULL UNIQUE,
+	roles auth.user_role[] NOT NULL DEFAULT ARRAY['ROLE_USER']::auth.user_role[]
+);
