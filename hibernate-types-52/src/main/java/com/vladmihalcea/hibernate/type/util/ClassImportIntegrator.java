17e44001f8526abc498bf9319a72e9cd1e9ea4fb
Vlad Mihalcea <mihalcea.vlad@gmail.com>
2019-12-08 22:10:05 +0200

    Add missing JavaDoc comments for the ClassImportIntegrator

--- hibernate-types-52/src/main/java/com/vladmihalcea/hibernate/type/util/ClassImportIntegrator.java
+++ hibernate-types-52/src/main/java/com/vladmihalcea/hibernate/type/util/ClassImportIntegrator.java
@@ -8,16 +8,36 @@ import org.hibernate.service.spi.SessionFactoryServiceRegistry;
 import java.util.List;
 
 /**
+ * The {@link ClassImportIntegrator} implements the Hibernate {@link Integrator} contract
+ * and allows you to provide a {@link List} of classes to be imported using their simple name.
+ *
+ * For instance, you could use a DTO simple class name, instead of the fully-qualified name
+ * when building a constructor expression in a JPQL query.
+ *
+ * For more details about how to use it, check out <a href="https://vladmihalcea.com/dto-projection-jpa-query/">this article</a> on <a href="https://vladmihalcea.com/">vladmihalcea.com</a>.
+ *
  * @author Vlad Mihalcea
  */
 public class ClassImportIntegrator implements Integrator {
 
 	private final List<? extends Class> classImportList;
 
+	/**
+	 * Builds a new integrator that can register the provided classes.
+	 *
+	 * @param classImportList list of classes to be imported
+	 */
 	public ClassImportIntegrator(List<? extends Class> classImportList) {
 		this.classImportList = classImportList;
 	}
 
+	/**
+	 * Register the provided classes by their simple name.
+	 *
+	 * @param metadata metadata
+	 * @param sessionFactory Hibernate session factory
+	 * @param serviceRegistry Hibernate service registry
+	 */
 	@Override
 	public void integrate(
 			Metadata metadata,
@@ -31,6 +51,9 @@ public class ClassImportIntegrator implements Integrator {
 		}
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void disintegrate(
 			SessionFactoryImplementor sessionFactory,
