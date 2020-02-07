6ab860604b4a972233798d6b4b35f7088d3e8955
Vlad Mihalcea <mihalcea.vlad@gmail.com>
2020-02-07 21:13:24 +0200

    Add new test case for mapping JsonNode with the JsonBinaryType

---
+++ hibernate-types-52/src/test/java/com/vladmihalcea/hibernate/type/json/PostgreSQLJsonNodeTypeTest.java
@@ -0,0 +1,112 @@
+package com.vladmihalcea.hibernate.type.json;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.vladmihalcea.hibernate.type.json.internal.JacksonUtil;
+import com.vladmihalcea.hibernate.type.util.AbstractPostgreSQLIntegrationTest;
+import org.hibernate.Session;
+import org.hibernate.annotations.NaturalId;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
+import org.junit.Test;
+
+import javax.persistence.*;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author Vlad Mihalcea
+ */
+public class PostgreSQLJsonNodeTypeTest extends AbstractPostgreSQLIntegrationTest {
+
+    @Override
+    protected Class<?>[] entities() {
+        return new Class<?>[]{
+                Book.class
+        };
+    }
+
+    @Test
+    public void test() {
+
+        doInJPA(entityManager -> {
+
+            Book book = new Book();
+            book.setIsbn("978-9730228236");
+            book.setProperties(
+                JacksonUtil.toJsonNode(
+                    "{" +
+                    "   \"title\": \"High-Performance Java Persistence\"," +
+                    "   \"author\": \"Vlad Mihalcea\"," +
+                    "   \"publisher\": \"Amazon\"," +
+                    "   \"price\": 44.99" +
+                    "}"
+                )
+            );
+
+            entityManager.persist(book);
+        });
+
+        doInJPA(entityManager -> {
+            Session session = entityManager.unwrap(Session.class);
+            Book book = session
+                    .bySimpleNaturalId(Book.class)
+                    .load("978-9730228236");
+
+            LOGGER.info("Book details: {}", book.getProperties());
+
+            assertEquals(expectedPrice(), book.getProperties().get("price").asText());
+
+            book.setProperties(
+                JacksonUtil.toJsonNode(
+                    "{" +
+                    "   \"title\": \"High-Performance Java Persistence\"," +
+                    "   \"author\": \"Vlad Mihalcea\"," +
+                    "   \"publisher\": \"Amazon\"," +
+                    "   \"price\": 44.99," +
+                    "   \"url\": \"https://www.amazon.com/High-Performance-Java-Persistence-Vlad-Mihalcea/dp/973022823X/\"" +
+                    "}"
+                )
+            );
+        });
+    }
+
+    protected String initialPrice() {
+        return "44.99";
+    }
+
+    protected String expectedPrice() {
+        return "44.99";
+    }
+
+    @Entity(name = "Book")
+    @Table(name = "book")
+    @TypeDef(typeClass = JsonBinaryType.class, defaultForType = JsonNode.class)
+    public static class Book {
+
+        @Id
+        @GeneratedValue
+        private Long id;
+
+        @NaturalId
+        private String isbn;
+
+        @Column(columnDefinition = "jsonb")
+        private JsonNode properties;
+
+        public String getIsbn() {
+            return isbn;
+        }
+
+        public void setIsbn(String isbn) {
+            this.isbn = isbn;
+        }
+
+        public JsonNode getProperties() {
+            return properties;
+        }
+
+        public void setProperties(JsonNode properties) {
+            this.properties = properties;
+        }
+    }
+}
