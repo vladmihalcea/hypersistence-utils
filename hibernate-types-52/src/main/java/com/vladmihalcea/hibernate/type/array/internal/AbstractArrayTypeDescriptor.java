51c5d251c9042bc212c48cfe9f1d878ecb431f3a
Jan-Willem Gmelig Meyling <jan-willem@youngmediaexperts.nl>
2020-04-12 13:16:30 +0200

    Make AbstractArrayTypeDescriptor#getArrayObjectClass public #214

--- hibernate-types-52/src/main/java/com/vladmihalcea/hibernate/type/array/internal/AbstractArrayTypeDescriptor.java
+++ hibernate-types-52/src/main/java/com/vladmihalcea/hibernate/type/array/internal/AbstractArrayTypeDescriptor.java
@@ -37,7 +37,7 @@ public abstract class AbstractArrayTypeDescriptor<T>
         this.arrayObjectClass = arrayObjectClass;
     }
 
-    protected Class<T> getArrayObjectClass() {
+    public Class<T> getArrayObjectClass() {
         return arrayObjectClass;
     }
 
