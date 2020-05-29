59dec44e1886694d361563bb2d93bce147e6265a
Will Fisher <wfisher@lirr.org>
2020-05-29 13:20:11 -0400

    A ClassNotFoundException is thrown when using an Enum List or Array in Kotlin #238

--- hibernate-types-52/src/main/java/com/vladmihalcea/hibernate/type/array/internal/ListArrayTypeDescriptor.java
+++ hibernate-types-52/src/main/java/com/vladmihalcea/hibernate/type/array/internal/ListArrayTypeDescriptor.java
@@ -8,6 +8,7 @@ import org.hibernate.usertype.DynamicParameterizedType;
 import java.io.Serializable;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
+import java.lang.reflect.WildcardType;
 import java.util.*;
 
 /**
@@ -100,7 +101,11 @@ public class ListArrayTypeDescriptor extends AbstractArrayTypeDescriptor<Object>
         Type memberGenericType = ReflectionUtils.getMemberGenericTypeOrNull(entityClass, property);
         if (memberGenericType instanceof ParameterizedType) {
             ParameterizedType parameterizedType = (ParameterizedType) memberGenericType;
-            Class arrayElementClass = ReflectionUtils.getClass(parameterizedType.getActualTypeArguments()[0].getTypeName());
+            Type genericType = parameterizedType.getActualTypeArguments()[0];
+            if (genericType instanceof WildcardType) {
+                genericType = ((WildcardType) genericType).getUpperBounds()[0];
+            }
+            Class arrayElementClass = ReflectionUtils.getClass(genericType.getTypeName());
             setArrayObjectClass(
                 arrayElementClass.isArray() ?
                     arrayElementClass :
