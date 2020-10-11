f1196ab5e7a3d46c38217a821446903c04be44fb
freemahn <gordon.pav@gmail.com>
2020-10-11 19:09:32 +0200

    The NotSerializableException is thrown when mapping a Map<String, List<>> entity attribute to a JSON column #243

--- hibernate-types-52/src/test/java/com/vladmihalcea/hibernate/type/util/ObjectMapperJsonSerializerTest.java
+++ hibernate-types-52/src/test/java/com/vladmihalcea/hibernate/type/util/ObjectMapperJsonSerializerTest.java
@@ -1,10 +1,16 @@
 package com.vladmihalcea.hibernate.type.util;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotSame;
+
+import com.google.common.base.Objects;
+import com.google.common.collect.Lists;
+import org.hibernate.annotations.Type;
 import org.junit.Test;
 
+import javax.persistence.Column;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -52,7 +58,7 @@ public class ObjectMapperJsonSerializerTest {
 
     @Test
     public void should_clone_empty_collection() {
-        List<?> original = new ArrayList();
+        List<?> original = new ArrayList<>();
         Object cloned = serializer.clone(original);
         assertEquals(original, cloned);
         assertNotSame(original, cloned);
@@ -113,6 +119,35 @@ public class ObjectMapperJsonSerializerTest {
         assertEquals(original, cloned);
         assertNotSame(original, cloned);
     }
+
+    @Test
+    public void should_clone_serializable_complex_object_with_serializable_nested_object() {
+        Map<String, List<SerializableObject>> map = new LinkedHashMap<>();
+        map.put("key1", Lists.newArrayList(new SerializableObject("name1")));
+        map.put("key2", Lists.newArrayList(
+                new SerializableObject("name2"),
+                new SerializableObject("name3")
+        ));
+        Object original = new SerializableComplexObject(map);
+        Object cloned = serializer.clone(original);
+        assertEquals(original, cloned);
+        assertNotSame(original, cloned);
+    }
+
+    @Test
+    public void should_clone_serializable_complex_object_with_non_serializable_nested_object() {
+        Map<String, List<NonSerializableObject>> map = new LinkedHashMap<>();
+        map.put("key1", Lists.newArrayList(new NonSerializableObject("name1")));
+        map.put("key2", Lists.newArrayList(
+                new NonSerializableObject("name2"),
+                new NonSerializableObject("name3")
+        ));
+        Object original = new SerializableComplexObjectWithNonSerializableNestedObject(map);
+        Object cloned = serializer.clone(original);
+        assertEquals(original, cloned);
+        assertNotSame(original, cloned);
+    }
+
 
     private static class SerializableObject implements Serializable {
         private final String value;
@@ -179,6 +214,80 @@ public class ObjectMapperJsonSerializerTest {
         @Override
         public String toString() {
             return value;
+        }
+    }
+
+    private static class SerializableComplexObject implements Serializable {
+
+        @Type(type = "jsonb")
+        @Column(columnDefinition = "jsonb")
+        private final Map<String, List<SerializableObject>> value;
+
+        private SerializableComplexObject(@JsonProperty("value") Map<String, List<SerializableObject>> value) {
+            this.value = value;
+        }
+
+        public Map<String, List<SerializableObject>> getValue() {
+            return value;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o)
+                return true;
+            if (o == null || getClass() != o.getClass())
+                return false;
+
+            SerializableComplexObject that = (SerializableComplexObject) o;
+
+            return value.equals(that.value);
+        }
+
+        @Override
+        public int hashCode() {
+            return value.hashCode();
+        }
+
+        @Override
+        public String toString() {
+            return value.toString();
+        }
+    }
+
+    private static class SerializableComplexObjectWithNonSerializableNestedObject implements Serializable {
+
+        @Type(type = "jsonb")
+        @Column(columnDefinition = "jsonb")
+        private final Map<String, List<NonSerializableObject>> value;
+
+        private SerializableComplexObjectWithNonSerializableNestedObject(@JsonProperty("value") Map<String, List<NonSerializableObject>> value) {
+            this.value = value;
+        }
+
+        public Map<String, List<NonSerializableObject>> getValue() {
+            return value;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o)
+                return true;
+            if (o == null || getClass() != o.getClass())
+                return false;
+
+            SerializableComplexObjectWithNonSerializableNestedObject that = (SerializableComplexObjectWithNonSerializableNestedObject) o;
+
+            return value.equals(that.value);
+        }
+
+        @Override
+        public int hashCode() {
+            return value.hashCode();
+        }
+
+        @Override
+        public String toString() {
+            return value.toString();
         }
     }
 }
