263c6340ca8622805d9a66c34014bfff0ef38075
Vlad Mihalcea <mihalcea.vlad@gmail.com>
2022-08-15 20:55:48 +0300

    Add Oracle and SQL Server to Docker Compose

--- docker/mysql/docker-entrypoint-initdb.d/init-db.sql
+++
@@ -1,4 +0,0 @@
-CREATE DATABASE high_performance_java_persistence;
-use high_performance_java_persistence;
-
-GRANT ALL PRIVILEGES ON high_performance_java_persistence.* TO 'mysql'@'%' IDENTIFIED BY 'admin';
--- docker/postgresql/docker-entrypoint-initdb.d/init-db.sql
+++
@@ -1,5 +0,0 @@
-alter role postgres password 'admin';
-create database high_performance_java_persistence with owner postgres;
-
-\c high_performance_java_persistence postgres;
-
