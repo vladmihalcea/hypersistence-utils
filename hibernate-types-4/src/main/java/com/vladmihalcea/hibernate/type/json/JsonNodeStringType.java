d6a1eca95f0f44fc6bab18a3a8c2edd1f3356de7
Vlad Mihalcea <mihalcea.vlad@gmail.com>
2022-08-15 20:13:17 +0300

    Fix Javadoc issues

--- hibernate-types-4/src/main/java/com/vladmihalcea/hibernate/type/json/JsonNodeStringType.java
+++ hibernate-types-4/src/main/java/com/vladmihalcea/hibernate/type/json/JsonNodeStringType.java
@@ -18,10 +18,6 @@ import com.vladmihalcea.hibernate.type.util.ObjectMapperWrapper;
  * <p>
  * For more details about how to use it, check out <a href="https://vladmihalcea.com/how-to-store-schema-less-eav-entity-attribute-value-data-using-json-and-hibernate/">this article</a> on <a href="https://vladmihalcea.com/">vladmihalcea.com</a>.
  * </p>
- * <p>
- * If you want to use a more portable Hibernate <code>Type</code> that can work on <strong>Oracle</strong>, <strong>SQL Server</strong>, <strong>PostgreSQL</strong>, <strong>MySQL</strong>, or <strong>H2</strong> without any configuration changes, then you should use the {@link JsonType} instead.
- * </p>
- *
  * @author Vlad Mihalcea
  */
 public class JsonNodeStringType extends AbstractHibernateType<JsonNode> {
